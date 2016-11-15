3d46d9b023f1e0b48e4a3f31425028df24100e4d
王乐 <e.le.lee.leee@gmail.com>
2016-11-15 14:13:13 +0800

    change

--- PyBasic/Basic_1/operation.py
+++ PyBasic/Basic_1/operation.py
@@ -1,17 +1,9 @@
 # coding=utf-8
 from __future__ import print_function
 from __future__ import division
-import os,sys
-import PyBasic.Sort
 
 # 除法包：
 # from __future__ import division
-#print(1 / 2)
+print(1 / 2)
 #整除
-#print(1 // 2)
-
-
-# print (str1+str2+str1)
-
-str1= '\''
-os.system('jps')
+print(1 // 2)
