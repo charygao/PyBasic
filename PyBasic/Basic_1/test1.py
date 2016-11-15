0f0fdae929cd40143e0e1f09dd00159fad7144c2
王乐 <e.le.lee.leee@gmail.com>
2016-11-15 11:26:14 +0800

    Bubble sort

---
+++ PyBasic/Basic_1/test1.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from __future__ import print_function
+from __future__ import division
+import os
+import PyBasic.Sort
+
+# 除法包：
+# from __future__ import division
+#print(1 / 2)
+#整除
+#print(1 // 2)
+
+
+# print (str1+str2+str1)
+
+str1= '\''
+os.system('jps')
+
