be7717f7b3af794ba925f42aa6e27df65f47db81
王乐 <e.le.lee.leee@gmail.com>
2016-11-16 11:44:03 +0800

    Make Data

--- PyBasic/Basic_2/RandomTest.py
+++ PyBasic/Basic_2/RandomTest.py
@@ -1,20 +1,54 @@
 # coding=utf-8
 from __future__ import print_function
 import random
+import threading
 
-for num in range(10):
+
+def newData():
     # 顾客定义
-    guests = [random.randint(1, 1000000000000) for _ in range(1)]
+    guests = [random.randint(1, 9999999999) for _ in range(1)]
     # 点菜
-    foods = [random.randint(1, 30) for _ in range(10)]
-    # print(guests + foods)
+    foods = [random.randint(1, 30) for _ in range(random.randint(1, 20))]
+    return str(guests + foods) + "\n"
+
+
+# 写入
+def writeData(file,num):
+    count = 0
+    while count < num:
+        try:
+            file.write(newData())
+            count += 1
+        except Exception as e:
+            print()
+            'write warn log error', str(e)
+            break
+    print()
+    'write data finished'
 
 
 def main():
-    fout = open("/home/lele/文档/Codes/Pycharm/PyBasic/PyBasic/PyBasic/Basic_2/test.txt", "w")
+    global f
+    fileName = 'Datas/orders.txt'
+    # 模式：追加
+    mode = 'w+'
+    # 生成的数据量
+    dataNum=1000
+    # 创建线程来写文件
+    try:
+        f = open(fileName, mode)
+        t = threading.Thread(target=writeData, args=[f,dataNum])
+        t.start()
+        t.join()
 
-    for i in range(5):
-        line = str(i) + "\n"
-        fout.write(line)
+    except Exception as e:
+        print()
+        'write data failed,', str(e)
+    finally:
+        f.close()
+    print()
+    'write data finished'
 
-    fout.close()
+
+if __name__ == '__main__':
+    main()
